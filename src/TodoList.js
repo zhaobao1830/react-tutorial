6ae64e58527c5f7a260ceb236511b6aea2f15c76
zhaobao1830 <zgt..5219438>
2020-10-22 11:05:45 +0800

    jsx语法细节

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,8 +5,8 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: '111',
-      list: []
+      inputValue: '',
+      list: ['学英语', '学语文']
     }
   }
 
@@ -14,15 +14,26 @@ class TodoList extends Component {
     return (
       <Fragment>
         <div>
+          <label htmlFor="insertArea">输入内容</label>
           <input
+            id="insertArea"
             value={this.state.inputValue}
             onChange={this.handleInputChange.bind(this)}
           />
-          <button>提交</button>
+          <button onClick={this.handleBtnClick.bind(this)}>提交</button>
         </div>
         <ul>
-          <li>学英语</li>
-          <li>学语文</li>
+          {
+           this.state.list.map((item, index) => {
+             return (
+               <li
+                 key={index}
+                 onClick={this.handleBtnDelete.bind(this, index)}
+                 dangerouslySetInnerHTML={{__html: item}}
+               />
+             )
+           })
+          }
         </ul>
       </Fragment>
     )
@@ -31,6 +42,22 @@ class TodoList extends Component {
   handleInputChange(e) {
     this.setState({
       inputValue: e.target.value
+    })
+  }
+
+  handleBtnClick(e) {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+
+  handleBtnDelete(index) {
+    console.log(index)
+    const list = [...this.state.list]
+    list.splice(index, 1)
+    this.setState({
+      list
     })
   }
 }
