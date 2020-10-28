b783e8a87aabaf712b8048b4f6dc411fd646ec04
zhaobao1830 <zgt..5219438>
2020-10-28 10:19:15 +0800

    优化代码

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,11 @@ const defaultState = {
   list: []
 }
 
+//  reducer是接受state的，然后返回新的数据，注意：不会修改state。
+// 是store自己拿到数据，对这个数据进行更新
+
+// export default返回的是纯函数
+// 纯函数指的是，给定固定的输入，就一定会有固定的输出，而且不会有让任何副作用
 export default (state = defaultState, action) => {
   if (action.type === CHANGE_INPUT_VALUE) {
     const newState = JSON.parse(JSON.stringify(state));
