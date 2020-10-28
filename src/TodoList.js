2a5cb10bc0cad61c3ce15833cfc4b797f3bfa781
zhaobao1830 <zgt..5219438>
2020-10-28 09:03:25 +0800

    新建actionCreators.js，将acion方法抽取出来

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,13 +2,12 @@ import React, { Component } from "react";
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd';
 import store from "./store";
-import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM } from './store/actionTypes'
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from "./store/actionCreators";
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = store.getState()
-    // this.handleStoreChange = this.handleStoreChange.bind(this)
     store.subscribe(this.handleStoreChange)
   }
   render() {
@@ -54,11 +53,7 @@ class TodoList extends Component {
   }
 
   handleInputChange = (e) => {
-    const action = {
-      type: CHANGE_INPUT_VALUE,
-      value: e.target.value
-    }
-
+    const action = getInputChangeAction(e.target.value)
     store.dispatch(action)
   }
 
@@ -67,19 +62,12 @@ class TodoList extends Component {
   }
 
   handleBtnClick = () => {
-    const action = {
-      type: ADD_TODO_ITEM
-    }
-
+    const action = getAddItemAction()
     store.dispatch(action)
   }
 
   handleDeleteClick = (index) => {
-    const action = {
-      type: DELETE_TODO_ITEM,
-      index
-    }
-
+    const action = getDeleteItemAction(index)
     store.dispatch(action)
   }
 }
